d2dedfbc21e180259b24145ecf32da6deaac2f51
batuhan <mnf_41@hotmail.com>
2021-07-18 13:00:07 +0300

    card update

--- lib/view/character_name_view.dart
+++ lib/view/character_name_view.dart
@@ -6,22 +6,27 @@ class CharaterName extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetBuilder<CharacterViewModel>(builder: (builder) {
-      double wSize = MediaQuery.of(context).size.width;
+      //  double wSize = MediaQuery.of(context).size.width;
       return ListView.builder(
-        itemCount: builder.characterList.length,
+        itemCount: 1,
         itemBuilder: (BuildContext context, int index) {
           return InkWell(
             onTap: () {},
             child: Card(
-              child: ListTile(
-                leading: Image.network("${builder.characterList[index].image}"),
-                title: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(builder.characterList[index].name.toString()),
-                  ],
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              child: Container(
+                child: ListTile(
+                  leading:
+                      Image.network("${builder.characterList[index].image}"),
+                  title: Row(
+                    children: [
+                      Text(builder.characterList[index].name.toString()),
+                    ],
+                  ),
+                  trailing:
+                      Text(builder.characterList[index].status.toString()),
                 ),
-                trailing: Text(builder.characterList[index].status.toString()),
               ),
             ),
           );

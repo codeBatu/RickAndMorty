25d316a6a350f459f8fe6dc90baecab20f92479c
batuhan <mnf_41@hotmail.com>
2021-07-19 11:15:06 +0300

    main

--- lib/view/character_name_view.dart
+++ lib/view/character_name_view.dart
@@ -1,45 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_state_manager/src/simple/get_state.dart';
-import 'package:rickandmorty/view_model/character_view_model.dart';
+
+import 'package:rickandmorty/widget/main_list_page_widget.dart';
 
 class CharaterName extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<CharacterViewModel>(builder: (builder) {
-      //  double wSize = MediaQuery.of(context).size.width;
-      return ListView.builder(
-        itemCount: builder.characterList.length,
-        itemBuilder: (BuildContext context, int index) {
-          return InkWell(
-            onTap: () {},
-            child: Card(
-              color: Colors.orange,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black, width: 2),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: ListTile(
-                  leading: Image.network(
-                    "${builder.characterList[index].image}",
-                    height: 50,
-                  ),
-                  title: Row(
-                    children: [
-                      Text(builder.characterList[index].name.toString()),
-                    ],
-                  ),
-                  trailing:
-                      Text(builder.characterList[index].status.toString()),
-                ),
-              ),
-            ),
-          );
-        },
-      );
-    });
+    return ListPage();
   }
 }
